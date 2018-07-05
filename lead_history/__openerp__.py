e411a4dfa425ad471144dd34e63eef37ad9d855c
Debasish-FDS <ddashcool@gmail.com>
2018-07-05 13:12:32 +0530

    4th commit

---
+++ lead_history/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lead History",
+    'summary': """It fetch the history of a lead""",
+    'description': """
+
+=======================
+
+1. This module show the history of the lead. When any lead update it shows, which field updated with field value.
+2. You can print all that history of lead as a report.
+3. You can find Lead History menu under Report Section of Sales menu.
+
+   Sales > Reports > Lead History
+    """,
+    'author': "Debasisa Dash",
+    'website': "http://www.fdshive.com",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base','web','crm'],
+    'data': [
+        'view/lead_history_view.xml',
+        'view/lead_history_report.xml',
+        'view/report_lead.xml',
+        'security/ir.model.access.csv',
+    ],
+
+    'demo': [
+        ],
+    'installable':True,
+    'auto_install':False,
+}
